1541c1520e12841626ef9be5c6fb21c63465a383
MÜLLER IGAZ ALEX <50291971@ort>
2026-04-17 10:10:41 -0300

    GUIA 1

---
+++ Assets/Scripts/DatosDelUsuario.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DatosDelUsuario : MonoBehaviour
+{
+    public string nombre;
+    public int edad;
+    public float estatura;
+    public bool donantedeorganos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Mi nombre es " + nombre + " tengo " + edad + " , mido " + estatura + " metros y es " + donantedeorganos + " que soy donante de organos.");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/DeclaracionDeVariables.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeclaracionDeVariables : MonoBehaviour
+{
+    public int numero;
+    public float decimales;
+    public string palabra;
+    public bool efecto;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log(numero);
+        Debug.Log(decimales);
+        Debug.Log(palabra);
+        Debug.Log(efecto);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
